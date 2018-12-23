e08f38b1cea1c4f07d3ceba160d09fbf4373765f
Shuntaro Tsunoda <shun.tsunoda@gmail.com>
2018-12-23 11:25:40 +0900

    Fix compile_exec_array

--- sources/forth_modoki/interpreter/eval.c
+++ sources/forth_modoki/interpreter/eval.c
@@ -39,6 +39,47 @@ static void div_op() {
    two_op( / );
 }
 
+static void register_primitives() {
+   Token_t def = {.ltype=ELEMENT_C_FUNC, .u.cfunc=def_op};
+   dict_put("def", &def);
+
+   Token_t add = {.ltype=ELEMENT_C_FUNC, .u.cfunc=add_op};
+   dict_put("add", &add);
+
+   Token_t sub = {.ltype=ELEMENT_C_FUNC, .u.cfunc=sub_op};
+   dict_put("sub", &sub);
+
+   Token_t mul = {.ltype=ELEMENT_C_FUNC, .u.cfunc=mul_op};
+   dict_put("mul", &mul);
+
+   Token_t div = {.ltype=ELEMENT_C_FUNC, .u.cfunc=div_op};
+   dict_put("div", &div);
+}
+
+#define MAX_NAME_OP_NUMBERS 256
+static struct TokenArray* compile_exec_array(int prev_ch) {
+   int ch = prev_ch;
+   int num_token = 0;
+   Token_t token = {UNKNOWN, {0}};
+   Token_t arr[MAX_NAME_OP_NUMBERS];
+   struct TokenArray* ta;
+
+   for (int i = 0; i < MAX_NAME_OP_NUMBERS; i++) {
+      ch = parse_one(ch, &token);
+      if (token.ltype == CLOSE_CURLY) {
+         num_token = i;
+         break;
+      }
+      arr[i] = token;
+   }
+   ta = malloc(sizeof(struct TokenArray) + sizeof(Token_t) * num_token);
+   ta->len = num_token;
+   for (int i = 0; i < num_token; i++) {
+      ta->token[i] = arr[i];
+   }
+   return ta;
+}
+
 //
 // eval
 //
@@ -46,7 +87,7 @@ static void div_op() {
 void eval() {
    int ch = EOF;
    Token_t token = {UNKNOWN, {0}};
-   struct TokenArray *exec_arr;
+   struct TokenArray* exec_arr;
 
    do {
       ch = parse_one(ch, &token);
@@ -70,10 +111,11 @@ void eval() {
                stack_push(&token);
                break;
             case OPEN_CURLY:
-               ch = compile_exec_array(exec_arr);
+               exec_arr = compile_exec_array(ch);
                token.ltype = EXEC_ARRAY;
                token.u.bytecodes = exec_arr;
                stack_push(&token);
+               ch = CLOSE_CURLY;
                break;
             default:
                /* printf("Unknown type %d\n", token.ltype); */
@@ -83,53 +125,31 @@ void eval() {
    } while(ch != EOF);
 }
 
-static void register_primitives() {
-   Token_t def = {.ltype=ELEMENT_C_FUNC, .u.cfunc=def_op};
-   dict_put("def", &def);
-
-   Token_t add = {.ltype=ELEMENT_C_FUNC, .u.cfunc=add_op};
-   dict_put("add", &add);
-
-   Token_t sub = {.ltype=ELEMENT_C_FUNC, .u.cfunc=sub_op};
-   dict_put("sub", &sub);
-
-   Token_t mul = {.ltype=ELEMENT_C_FUNC, .u.cfunc=mul_op};
-   dict_put("mul", &mul);
-
-   Token_t div = {.ltype=ELEMENT_C_FUNC, .u.cfunc=div_op};
-   dict_put("div", &div);
-}
-
-#define MAX_NAME_OP_NUMBERS 256
-int compile_exec_array(struct TokenArray *out_token_array) {
-   int ch = EOF;
-   int num_token = 0;
-   Token_t token = {UNKNOWN, {0}};
-   Token_t arr[MAX_NAME_OP_NUMBERS];
-
-   for (int i = 0; i < MAX_NAME_OP_NUMBERS; i++) {
-      ch = parse_one(ch, &token);
-      if (token.ltype == CLOSE_CURLY) {
-         num_token = i;
-         break;
-      }
-      arr[i] = token;
-   }
-   out_token_array = malloc(sizeof(struct TokenArray) + sizeof(Token_t) * num_token);
-   out_token_array->len = num_token;
-   for (int i = 0; i < num_token; i++) {
-      out_token_array->token[i] = arr[i];
-   }
-   return ch;
-}
-
 //
 // Unit test
 //
 
-static void assert_token_number(int expect, Token_t *actual) {
+static void assert_token_number(int expect, Token_t* actual) {
    assert(actual->ltype == NUMBER);
    assert(actual->u.number == expect);
+}
+
+static void assert_token_array(struct TokenArray* expect, struct TokenArray* actual);
+static void assert_token(Token_t expect, Token_t actual) {
+   assert(expect.ltype == actual.ltype);
+   if (actual.ltype == NUMBER) {
+      assert(expect.u.number == actual.u.number);
+   } else if (actual.ltype == EXECUTABLE_NAME || actual.ltype == LITERAL_NAME) {
+      assert(strcmp(expect.u.name, actual.u.name) == 0);
+   } else if (actual.ltype == EXEC_ARRAY) {
+      assert_token_array(expect.u.bytecodes, actual.u.bytecodes);
+   }
+}
+
+static void assert_token_array(struct TokenArray* expect, struct TokenArray* actual) {
+   for (int i = 0; i < actual->len; i++) {
+      assert_token(expect->token[i], actual->token[i]);
+   }
 }
 
 static void test_eval_num_one() {
@@ -243,9 +263,12 @@ static void test_eval_num_div() {
 }
 
 static void test_eval_exec_array() {
-   char *input = "{ 1 }";
-   struct TokenArray bc = {1, {{NUMBER, {1}}}};
-   Token_t expect = {.ltype=EXEC_ARRAY, .u.bytecodes=&bc};
+   char *input = "{1}";
+   struct TokenArray* bc = malloc(sizeof(struct TokenArray) + sizeof(Token_t) * 1);
+   bc->len = 1;
+   Token_t t0 = {NUMBER, {1}};
+   bc->token[0] = t0;
+   Token_t expect = {.ltype=EXEC_ARRAY, .u.bytecodes=bc};
 
    cl_getc_set_src(input);
 
@@ -253,6 +276,7 @@ static void test_eval_exec_array() {
 
    Token_t actual = {UNKNOWN, {0}};
    stack_pop(&actual);
+   assert_token(expect, actual);
 
    stack_clear();
 }
@@ -263,15 +287,14 @@ static void test_eval_exec_array() {
 
 int main() {
    register_primitives();
-   // test_eval_num_one();
-   // test_eval_num_two();
-   // test_eval_num_add();
-   // test_eval_literal();
-   // test_eval_num_sub();
-   // test_eval_num_mul();
-   // test_eval_num_div();
+   test_eval_num_one();
+   test_eval_num_two();
+   test_eval_num_add();
+   test_eval_literal();
+   test_eval_num_sub();
+   test_eval_num_mul();
+   test_eval_num_div();
    test_eval_exec_array();
-   return 1;
 
    /* cl_getc_set_src("1 2 3 add add 4 5 6 7 8 9 add add add add add add"); */
    cl_getc_set_src("/foo 55 def /bar 11 def 1 foo add bar add 1 sub 11 div");
